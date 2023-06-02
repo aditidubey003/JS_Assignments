2621259350dc3b62599dde6f1fd41bccbf5fea3f
Aditi Dubey <aditidubey576@gmail.com>
2023-06-02 23:10:28 +0530

    completed the loops part- 2 Ass.

--- Value, Varaibles, Types and Operator/06- Loops2/loops.js
+++ Value, Varaibles, Types and Operator/06- Loops2/loops.js
@@ -6,18 +6,36 @@
 - do after each iteration
 */
 //For printing numbers from 10 to 0
+for(let i = 10; i >= 0; i--){
+    console.log(i, "reverse manner");
+}
+
+
 
 // For printing all the even numbers from 1 to 10
-
+for(let i = 0; i <= 10; i++){
+    if( i % 2 == 0){
+     console.log(i, "even");
+    }
+}
 
 // For printing all the odd numbers from 1 to 10
+for(let i = 1; i <= 10; i++){
+    if( i % 2 !== 0){
+       console.log(i, "odds");
+    }
+}
 
 
 //For printing all the odd numbers from 1 to 10 in reverse manner
-
+for(let i = 10; i >= 1; i--){
+    console.log(i, "reverse manner");
+}
 
 // For Printing my name 5 times
-
+for(let i = 1; i <= 5; i++){
+    console.log("Aditi Dubey");
+}
 
 
 //While loop
@@ -25,13 +43,28 @@
 if the condition is true the loop will follow otherwise it will break */
 
 // For loggin all the no's between 1 to 10
-
+let i = 1;
+while(i < 10){
+    console.log(i, "by while loop");
+    i++
+}
 
 
 // For logging all the even numbewrs from 1 to 10
-
-
+let a = 1;
+while(a < 10){
+    if(a % 2 == 0){
+        console.log(a, "by while loop");
+    }
+    a++;
+}
 
 
 // For getting the multiply of Numbers from  1 to 10
-
+let b = 1;
+let product = 1;
+while(b <= 10){
+    product = product * b;
+    console.log(product, "product");
+    b++;
+}
