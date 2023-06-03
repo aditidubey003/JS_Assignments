df0e3048f986872f7f63688ae1e9286cd30203ee
Aditi Dubey <aditidubey576@gmail.com>
2023-06-03 10:15:44 +0530

    Assignment:Level 2

--- Value, Varaibles, Types and Operator/07- Ass level 2/code/index.js
+++ Value, Varaibles, Types and Operator/07- Ass level 2/code/index.js
@@ -6,27 +6,61 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user = prompt("Enter your Name");
+alert(user);
+let userName = user;
+console.log(user, '1');
+console.log(userName, '2');
+let age = 22, isLearning = true;
+user = 'John';
+console.log(user, '3');
+console.log(userName, '4');
 
+
+// Confusion in above
 
 
 /*
 2.Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
 
+let numA = Number(prompt("Enter first No."));
+let numB = Number(prompt("Enter Second No."));
+let sum  = numA + numB;
+alert(sum);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
-
+for(let c = 0; c <= 10;c = c + 2){
+    console.log(c, "value of c")
+}
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
-
+for(let i = 1; i <= 10; i++){
+    if( i % 2 !== 0){
+        console.log(i, "odds");
+    }
+}
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
-
-
+let string = "";
+let i = 5;
+while(i >= 0){
+    string = string + i;
+    console.log(string);
+    i--;
+}
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
-
+let stringOutput = "";
+let  a = 10;
+while(a >= 0){
+    if(a % 2 == 0){
+        stringOutput = stringOutput + a;
+        console.log(stringOutput); 
+    }
+    a--;
+}
 
 
 // Comparison
@@ -43,6 +77,20 @@ Example:
 "hello", -21; // false
 
 */
+let num1 = Number(prompt("Enter first No."));
+let num2 = Number(prompt("Enter Second No."));
+
+if(num1 === num2){
+    alert("they are equal");
+} else (num1 !== num2){
+    alert("they are not equal");
+// } else if(typeof(num1) || typeof(num2) !== number) {
+//     alert("Enter a valid value")
+}
+
+
+
+
 
 
 
