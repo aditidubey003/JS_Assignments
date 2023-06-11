ae157df2455c2f40613bae89f1785fe6527ade74
Aditi Dubey <aditidubey576@gmail.com>
2023-06-11 23:35:41 +0530

    Functions

---
+++ Working with the funcctions/02- Arguments and Return/code/index.js
@@ -0,0 +1,81 @@
+/*
+1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
+*/
+
+
+/*
+2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
+fullName.
+
+Example:
+  getFullName("John", "Snow"); // "John Snow"
+  getFullName("Nelson", "Mandela"); // "Nelson Mandela"
+*/
+
+getFullName ('Aditi', 'Dubey');
+getFullName ('S.K', 'Dubey');
+
+/*
+3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
+the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
+
+Example:
+addTwoNumbers(10, 22); // 32
+addTwoNumbers(20, 32); // 32
+addTwoNumbers(10, "100"); // Alert Enter Valid Input
+*/
+
+
+addTwoNumbers(12, 56);
+addTwoNumbers(17, 55);
+
+/*
+4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
+can only be one of these `add, sub, mul, div`. Based on the operator return the value. If the `numA` or `numB`
+is of any other data type alert `Enter Valid Input`
+
+Example:
+
+calc(10, 20, 'add'); // 30
+calc(20, 10, 'sub'); // 10
+calc(20, 10, 'mul'); // 200
+*/
+
+
+
+calc(5, 3, 'add');
+calc(3, 7, 'sub');
+calc(3, 3, 'mul');
+calc(10, 5, 'div');
+
+
+/*
+5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
+on if the year id leap year or not.
+
+isLeapYear(2000); // true
+isLeapYear(2001); // false
+*/
+
+isLeapYear (2005);
+isLeapYear(2021);
+isLeapYear(2000);
+
+
+
+//6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
+
+
+getFactorial(6);
+getFactorial(10);
+
+
+
+
+
+
+//Voting Age
+
+
+votingAge( 34);
+votingAge( 5);
---
+++ Working with the funcctions/03- Types of Functions/code/index.js
@@ -0,0 +1,234 @@
+// -ook at the example below and do the same for other problems.
+
+/*
+Write a function named convertToString that accepts a number and converts the number into a string (hint: use String()) and return the converted value.
+Do the following for the given problem:
+*/
+
+// - Write a Function Decleration
+
+  convertToString();
+
+// - Write a Function Expression
+
+// - Write an Arrow Function without curly brackets (if possible)
+
+
+// - Write an Arrow Function with curly brackets
+
+
+
+// - Execute the function
+
+
+// - Execute the function and store the return value in a variable.
+ let output = convertToString(21);
+
+// - What is the typeof returnValue
+// typeof returnValue is "string"
+
+/* 1. Add one to the given value
+
+Write a function named `addOne` that accepts a number, adds one to the given value and`return` it(hint: use return keyword).
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+
+
+// - Write a Function Expression
+
+
+// - Write an Arrow Function without curly brackets(if possible)
+
+
+// - Write an Arrow Function with curly brackets
+
+
+// - Execute the function
+
+addOne(5);
+
+// - Execute the function and store the return value in a variable.
+
+
+
+// - What is the typeof returnValue
+//typeof returnValue is "number"
+
+/* 2. Subtract one
+
+Write a function named `substractOne` that accepts a number, substracts one from the given value and return it. (hint: use return keyword)
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+
+
+
+// - Write a Function Expression
+
+
+// - Write an Arrow Function without curly brackets(if possible)
+
+
+
+
+// - Write an Arrow Function with curly brackets
+
+
+
+
+// - Execute the function
+
+substractOne(5);
+
+// - Execute the function and store the return value in a variable.
+
+
+
+// - What is the typeof returnValue
+//typeof returnValue is "number"
+
+
+/* 3. Add two numbers
+
+Write a function named `sum` that accepts two numbers and returns the sum of them.
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+
+
+// - Write a Function Expression
+
+
+
+// - Write an Arrow Function without curly brackets(if possible)
+
+
+// - Write an Arrow Function with curly brackets
+
+
+
+
+
+// - Execute the function
+
+sum(3,4);
+
+// - Execute the function and store the return value in a variable
+
+// - What is the typeof returnValue
+//typeof returnValue is "number"
+
+
+/* 4. Square of the given value
+
+Write a function named `square` that accepts a number and returns the square of the number.
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+
+
+// - Write a Function Expression
+
+
+
+// - Write an Arrow Function without curly brackets(if possible)
+
+
+
+// - Write an Arrow Function with curly brackets
+
+  
+
+
+
+// - Execute the function
+
+square(5);
+
+// - Execute the function and store the return value in a variable
+
+
+
+// - What is the typeof returnValue
+//typeof returnValue is "number"
+
+
+/* 5. Find isGreater
+
+Write a function named isGreater that accepts two numbers`x` and`y` and returns`true` if x is greater than y or return `false`.
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+
+
+
+// - Write a Function Expression
+
+
+
+// - Write an Arrow Function without curly brackets(if possible)
+
+
+// - Write an Arrow Function with curly brackets
+
+
+
+// - Execute the function
+
+isGreater(6,2);
+
+// - Execute the function and store the return value in a variable
+let greaterNum = isGreater(6,2);
+// - What is the typeof returnValue
+//typeof returnValue is "boolean"
+
+
+/* 6.
+
+Write a function named `oddOrEven` that accepts a`number` and returns
+
+  - `Number is odd` for odd number
+  - `Number is even` for even number.
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+
+
+// - Write an anonymous Function Expression
+
+
+// - Write an named Function Expression
+
+
+
+// - Write an Arrow Function without curly brackets (hint: use ternary operator)
+
+
+// - Write an Arrow Function with curly brackets
+
+
+
+// - Execute the function
+
+oddOrEven(5);
+
+// - Execute the function and store the return value in a variable
+
+
+
+// - What is the typeof returnValue
+//typeof returnValue is "string"
+
