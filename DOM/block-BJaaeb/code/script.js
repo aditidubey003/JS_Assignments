70b94df1c2c583423174ff8df0c7941794544670
Aditi Dubey <aditidubey576@gmail.com>
2023-08-05 10:04:59 +0530

    completeda aan assignment

--- DOM/block-BJaaeb/code/script.js
+++ DOM/block-BJaaeb/code/script.js
@@ -5,7 +5,15 @@ default value to be "text" and return the input element inside label. (create it
 
 */
 
+function createInputElm (labelMessage , type = 'text') {
+  let label = document.createElement('label')
+let input = document.createElement('input')
+input.type = type;
+label.innerText = labelMessage;
+ label.append(input);
 
+ return label;
+}
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
@@ -22,6 +30,15 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 
+function createList(array){
+let ul = document.createElement('ul')
+array.forEach(element => {
+  let li =  document.createElement('li')
+  li.innerText = element;
+  ul.append(li)
+});
+return ul;
+}
 
 
 
@@ -41,25 +58,56 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 </ul>
 */
 
-/*function createTodoList (array){
-  let ul = document.createElement('ul')
+// function createTodoList (array){
+//   let ul = document.createElement('ul')
  
-  let li = document.createElement('li')
-  li.append(span,input)
-  ul.append(li)
-  let span = document.createElement('span')
-  span.innerText = "X"
-  let input = document.createElement('input[type = "checkbox"]')
-  forEach((elm) => {
-    let p = document.createElement('p')
-    p.innerText = element
-  })
+//   let li = document.createElement('li')
+//   li.append(span,input)
+//   ul.append(li)
+//   let span = document.createElement('span')
+//   span.innerText = "X"
+//   let input = document.createElement('input[type = "checkbox"]')
+//   forEach((elm) => {
+//     let p = document.createElement('p')
+//     p.innerText = element
+//   })
   
+// }
+
+
+function createTodoList(array){
+  let ul = document.createElement('ul')
+  array.forEach(element => {
+    let li =  document.createElement('li')
+    ul.append(li);
+    let p = document.createElement('p')
+    p.innerText = element.name;
+    li.append(p);
+    let input = document.createElement('input')
+    input.checked = true
+    li.append(input)
+    let span = document.createElement('span')
+    if(element.isDone == false)
+    spanElm.innerText = '❌';
+    else
+    spanElm.innerText = '✅';
+    li.append(spanElm);
+  });
+  return ul;
+  };
+  
+
+
+
+
+
+
+
+
+function createTodoList (array){
+
 }
-*/
-
-
-// TEST
+TEST
 createTodoList([
   { name: 'Learn DOM', isDone: false },
   { name: 'Learn JS', isDone: true },
